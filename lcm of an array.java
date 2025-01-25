d53c135c59930db30640a2f6dedf8fd1304ee95f
Ganapathi442005 <147175511+Ganapathi442005@users.noreply.github.com>
2025-01-25 23:21:23 +0530

    Update lcm of an array.java

--- lcm of an array.java
+++ lcm of an array.java
@@ -1,49 +1,45 @@
 import java.util.*;
 class Main{
-      public static int gcd(int a,int b)
-      {
-          while(b!=0)
-          {
-              int temp=b;
-              b=a%b;
-              a=temp;
-          }
-          return a;
-      }
-      
-      
-      public static int lcm(int a,int b)
-      {
-          return (a*b)/gcd(a,b);
-      }
-      
-      
-      public static int lcmofarray(int[] ans)
-      {
-          int result=ans[0];
-          for(int i=1;i<ans.length;i++)
-          {
-             result=lcm(result,ans[i]);
-          }
-          return result;
-      }
-      
-      
-      
-      public static void main(String[] args)
-      {
-          Scanner sc=new Scanner(System.in);
-          System.out.println("enter the no of elements");
-          int n=sc.nextInt();
-          int[] arr=new int[n];
-          System.out.println("enter the elements");
-          for(int i=0;i<n;i++)
-          {
-              
-              arr[i]=sc.nextInt();
-          }
-          int a=lcmofarray(arr);
-         
-          System.out.println("the lcm of the array is:"+a);
-      }
+    public static void main(String[] args)
+    {
+        System.out.println("enter the no.of elements");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter the elements");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int d=lcmofarray(arr);
+        System.out.println("the lcm is:"+d);
+    }
+    
+    
+    public static int hcf(int a,int b)
+    {
+        while(b!=0)
+        {
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+    
+    
+    public static int lcm(int a,int b){
+        int c=(a*b)/hcf(a,b);
+        return c;
+    }
+    
+    public static int lcmofarray(int[] arr)
+    {
+       int result=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            result=lcm(result,arr[i]);
+        }
+        return result;
+    }
 }
