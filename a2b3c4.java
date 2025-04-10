df284a12dbb5f9b65a8d7c08bc9b290f63146635
Ganapathi442005 <147175511+Ganapathi442005@users.noreply.github.com>
2025-04-10 17:15:36 +0530

    Update a2b3c4.java

--- a2b3c4.java
+++ a2b3c4.java
@@ -17,3 +17,36 @@ public class Main {
     }
 }
 }
+//for two digit words
+*********************//
+    import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String : ");
+        String str = sc.nextLine();
+        int n = str.length();
+        int num = 0;
+        for(int i=0;i<n-1;i++){
+            num = num*10 + (str.charAt(i) - '0');
+            char c = str.charAt(i+1);
+            if("0123456789".indexOf(c)==-1){
+                String temp = "";
+                for(int j=0;j<num;j++){
+                    temp += c;
+                }
+                System.out.print(temp);
+                temp = "";
+                num = 0;
+                i++;
+            }
+        }
+        if(num!=0){
+            String temp = "";
+            for(int j=0;j<num;j++){
+                    temp += str.charAt(n-1);
+            }
+            System.out.print(temp);
+        }
+    }
+}
